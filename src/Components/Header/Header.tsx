c1bf02786518a113a9434a15f787fafb022471a9
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-04-24 12:57:46 +0300

    header mock btns

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,5 +1,27 @@
+import styles from './header.module.css'
+import { useNavigate } from 'react-router-dom';
+
+
+const buttons: {name:string,link: string}[] = [
+  {name: 'Мой блог', link: '/blog'},
+  {name: 'Новости', link: '/blog'},
+  {name: 'Друзья', link: '/blog'},
+]
+
 export function Header() {
+  const navigate = useNavigate()
+  const clickHandler = (link: string) => {
+    navigate(link)
+  }
   return (
-    <div>Хедер</div>
+    <ul className={styles.header}>
+      {buttons.map((el,index) => {
+        return (
+          <li key={index}>
+            <button onClick={() => clickHandler(`${el.link}`)}>{el.name}</button>
+          </li>
+        )
+      })}
+    </ul>
   );
 }
