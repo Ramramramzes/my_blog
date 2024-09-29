dd7b1e4388abeb534a5d0a31fb7e8766c84daf47
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-09-29 10:35:40 +0300

    feat: добавил страницу Login и роуты

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { Route, Routes } from "react-router-dom"
+import { Login } from "./pages/Login"
+
 export const App = () => {
   return (
-    <>App</>
+    <Routes>
+      <Route path="/" element={<Login />} />
+    </Routes>
   )
 }
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,7 @@
+// import styles from './login.scss';
+
+export function Login() {
+  return (
+    <>Login</>
+  );
+}
