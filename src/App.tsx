e082343175849e4070910fd2044915d370e51102
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-11-10 21:40:54 +0300

    feat: добавил сброс css

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import { Route, Routes } from "react-router-dom"
-import { Registrate } from "./pages/Registrate/Registrate"
-import { General } from "./pages/General/General"
-import { Login } from "./pages/Login/Login"
-import { RequireAuth } from "./hoks/RequireAuth"
+import { Route, Routes } from "react-router-dom";
+import { Registrate } from "./pages/Registrate/Registrate";
+import { General } from "./pages/General/General";
+import { Login } from "./pages/Login/Login";
+import { RequireAuth } from "./hocs/RequireAuth";
+import CssBaseline from '@mui/material/CssBaseline';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={
-          <Registrate /> 
-        }/>
-      <Route path="/login" element={
-          <Login />
-        } />
-      <Route path="/general" element={
-        <RequireAuth>
-          <General />
-        </RequireAuth>
-      } />
-    </Routes>
-  )
-}
+    <>
+      <CssBaseline />
+      <Routes>
+        <Route path="/" element={<Registrate />} />
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/general"
+          element={
+            <RequireAuth>
+              <General />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
