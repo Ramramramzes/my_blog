55c8cd310bcab555ec27ce5891dd325f2050b13b
Marat <68328146+Ramramramzes@users.noreply.github.com>
2025-02-02 20:07:22 +0300

    feat: добавил проксирование на новые ручки

--- env.d.ts
+++ env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_AUTH_BASEURL: string;
-  readonly VITE_WS_API: string;
+  readonly VITE_POSTS_BASEURL: string;
   readonly VITE_JWT_SECRET: string;
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
 dotenv.config();
 
-if (!process.env.VITE_PROXY_AUTH || !process.env.VITE_PROXY_WS) {
+if (!process.env.VITE_PROXY_AUTH || !process.env.VITE_PROXY_POSTS) {
   throw new Error("Не удалось найти необходимые переменные окружения VITE_PROXY_AUTH или VITE_PROXY_WS");
 }
 
 const proxyAuth = process.env.VITE_PROXY_AUTH;
-
+const proxyPosts = process.env.VITE_PROXY_POSTS;
 
 export default defineConfig({
   plugins: [react()],
@@ -19,6 +19,11 @@ export default defineConfig({
       '/logout': proxyAuth,
       '/refresh-token': proxyAuth,
       '/get-user': proxyAuth,
+      '/get-all-posts': proxyPosts,
+      '/get-user-posts': proxyPosts,
+      '/add-post': proxyPosts,
+      '/edit-post': proxyPosts,
+      '/delete-post': proxyPosts,
     }
   }
 })
